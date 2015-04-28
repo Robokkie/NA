7d5443e3974ec523a02b80fdc9378b25bc4a1ec9
Ryohei <trickhedgehog@gmail.com>
2015-04-28 17:40:01 +0900

    xを代入する場合の処理を修正

--- nibun.py
+++ nibun.py
@@ -9,8 +9,8 @@ class function:
 		self.b=b
 		self.c=c
 		self.d=d
-	def fnf(a,b,c,d,x):
-		return a*x*x*x+b*x*x+c*x+d
+	def fnf(self,x):
+		return self.a*x*x*x+self.b*x*x+self.c*x+self.d
 	def showinfo(self):
 		print 'a,b,c,d = %d %d %d %d'%(self.a, self.b, self.c, self.d)
 
@@ -21,6 +21,9 @@ func=function(a,b,c,d)
 func.showinfo()
 
 while True:
-	print "input e,f | f(e)*f(f)<0"
+	print "input e,f | e<f & f(e)*f(f)<0"
 	e,f = map(int,raw_input('input e,f=').split(","))
+	
+	if func.fnf(e)<func.fnf(f):
+		print "collect"
 	break
